d372fcfd9e1bea4e5d31b1672433f96bedfc27a9
Najinc <najib.chafei@etudiant.univ-reims.fr>
2025-05-24 00:35:16 +0200

    feat: starting history style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import History from './pages/History';
 
 function App() {
   return (
@@ -8,12 +9,14 @@ function App() {
       <nav className="bg-gray-800 text-white p-4 flex space-x-4">
         <Link to="/" className="hover:underline">Accueil</Link>
         <Link to="/about" className="hover:underline">À propos</Link>
+        <Link to="/history" className="hover:underline">Histoire</Link>
       </nav>
 
       <main className="mt-6">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/history" element={<History />} />
           {/* ajoute d’autres routes ici */}
         </Routes>
       </main>
