ef3a2d6e7875a70d452c4cdd70bee67d7ac82d05
Najinc <najib.chafei@etudiant.univ-reims.fr>
2025-05-24 03:44:17 +0200

    feat: improve history style

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -43,7 +43,20 @@ export default function History() {
       <div className="absolute inset-0 bg-[url('/lightning.png')] bg-cover bg-center opacity-10 animate-pulse-fast pointer-events-none z-0" />
 
       <section className="relative z-10 py-32 px-6 max-w-3xl mx-auto" data-scroll-section>
-        <h1 className="text-4xl font-bold text-cyan-400 mb-16 text-center drop-shadow-[0_0_10px_cyan]">
+
+      <div
+        className="absolute w-[30rem] h-[30rem] bg-cyan-400 rounded-full blur-3xl opacity-20 pointer-events-none animate-glow1 animate-glowFloat1"
+        data-scroll
+        data-scroll-speed="-1"
+        style={{ top: '20%', left: '10%' }}
+      />
+          
+        <div
+          className="absolute w-[30rem] h-[30rem] bg-fuchsia-500 rounded-full blur-3xl opacity-25 pointer-events-none animate-glow2"
+          data-scroll
+          data-scroll-speed="-2"
+        />
+        <h1 className="text-4xl font-accurastic text-cyan-400 mb-16 text-center drop-shadow-[0_0_10px_cyan]">
           Histoire de la Fête des Lumières
         </h1>
         <div className="timeline relative flex flex-col gap-32 border-l-4 border-purple-500/30 sm:border-l-0 sm:border-r-0 sm:before:absolute sm:before:left-1/2 sm:before:top-0 sm:before:bottom-0 sm:before:w-1 sm:before:bg-purple-500/40 pl-6 sm:pl-0 sm:before:translate-x-[-0.5px]">
@@ -59,11 +72,11 @@ export default function History() {
               data-scroll-speed={isLeft ? '1' : '2'}
               data-scroll-class="is-reveal"
             >
-              <div className={`absolute top-2 w-6 h-6 bg-purple-400 rounded-full shadow-[0_0_15px_#a855f7] ${isLeft ? "-right-3" : "-left-3"}`} />
+              <div className={`absolute top-2 w-6 h-6 bg-fuchsia-500 rounded-full shadow-[0_0_15px_#a855f7] ${isLeft ? "-right-3" : "-left-3"}`} />
 
               <div className={`sm:w-[45%] ${isLeft ? "text-right sm:pr-6" : "text-left sm:pl-6"}`}>
                 <h2 className="text-3xl font-extrabold text-cyan-300 drop-shadow-[0_0_10px_cyan] mb-2">{item.year}</h2>
-                <p className="text-lg text-purple-100 leading-relaxed font-outfit">{item.text}</p>
+                <p className="text-2xl text-purple-100 leading-relaxed font-nowaltlight">{item.text}</p>
               </div>
             </div>
           );
